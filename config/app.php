301264aadc3383e8ca17ab58528814fa574144df
Muhammad El-Badawy <elbadawy@LapStores-MacBook-Pro.local>
2018-06-02 05:47:23 +0200

    adding login and register api

--- config/app.php
+++ config/app.php
@@ -176,6 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // Passport Service Providers
+        Laravel\Passport\PassportServiceProvider::class,
+
 
     ],
 
